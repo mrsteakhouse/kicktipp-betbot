7bdc94790dba8c9d38c2f016b2b3bfb5b979e33e
Fabian Frey <mrsteakhouse1@gmail.com>
2026-03-03 14:14:19 +0100

    feat(*): add dev setup

--- shell.nix
+++ shell.nix
@@ -1,9 +1,27 @@
-{ pkgs ? import <nixpkgs> {}}:
+{
+  system ? builtins.currentSystem,
+  pkgs ? import <nixpkgs> { inherit system; },
+  ...
+}:
+let
+  pythonEnv = pkgs.python313.withPackages (
+    ps: with ps; [
+      numpy
+      scipy
+      # matplotlib
+      pandas
+      # jupyter
+      pip
+    ]
+  );
 
+in
 pkgs.mkShell {
-  packages = with pkgs; [
-		python313
-		python313Packages.pip
-        python313Packages.numpy
-    ];
+  packages = [
+    pythonEnv
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="$NIX_LD_LIBRARY_PATH:$LD_LIBRARY_PATH"
+  '';
 }
